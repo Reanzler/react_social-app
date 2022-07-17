1cdd6da90b78c98e86c2b012b3b5a229d9f49b23
Reanzler <maincontrol.rus@mail.ru>
2022-07-17 17:10:34 +0300

    added header component

---
+++ react_social-network/src/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+    return(
+        <div>
+            <a href = "#">Home</a>
+            <a href = "#">News Feed</a>
+            <a href = "#">Messages</a>
+        </div>
+    )
+}
+
+export {Header}
