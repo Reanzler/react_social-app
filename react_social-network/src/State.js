ace7e0db63759c48342135010b29e11d1a28dbd2
Reanzler <maincontrol.rus@mail.ru>
2022-09-07 23:19:17 +0300

    Added < store > which unites state and functions of BLL

--- react_social-network/src/State.js
+++ react_social-network/src/State.js
@@ -1,57 +1,60 @@
-let rerenderEntireTree = () => {console.log('test')}
-
-let state = {
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Barbara' },
-            { id: 2, name: 'Vera'},
-            { id: 3, name: 'Guly' },
-            { id: 4, name: 'Ivan' },
-            { id: 5, name: 'Sasha' },
-            { id: 6, name: 'Marat' },
-        ],
-        messages: [
-            {id: 1, message: 'Hi, buddy!'},
-            {id: 1, message: 'Hi, buddy!'},
-            {id: 1, message: 'Hi, buddy!'},
-            {id: 2, message: 'How are u, Baby?'},
-            {id: 3, message: 'Я тут!'},
-            {id: 3, message: 'Я тут!'},
-            {id: 3, message: 'Я тут!'}
-        ],
+let store = {
+        _state: {
+            dialogsPage: {
+                    dialogs: [
+                        { id: 1, name: 'Barbara' },
+                        { id: 2, name: 'Vera'},
+                        { id: 3, name: 'Guly' },
+                        { id: 4, name: 'Ivan' },
+                        { id: 5, name: 'Sasha' },
+                        { id: 6, name: 'Marat' },
+                    ],
+                    messages: [
+                        {id: 1, message: 'Hi, buddy!'},
+                        {id: 1, message: 'Hi, buddy!'},
+                        {id: 1, message: 'Hi, buddy!'},
+                        {id: 2, message: 'How are u, Baby?'},
+                        {id: 3, message: 'Я тут!'},
+                        {id: 3, message: 'Я тут!'},
+                        {id: 3, message: 'Я тут!'}
+                    ],
+            },
+            profilePage: {
+                    postsData: [
+                        {id: 1, message: 'Hi, How are u ?', likesCount: 12, DislikesCount: 2},
+                        {id: 2, message: 'It is my first post!)', likesCount: 78, DislikesCount: 0},
+                        {id: 3, message: 'Я тут!'}
+                    ],
+                    newPostText: ''
+            },
+            sidebar: {},
     },
-    profilePage: {
-            postsData: [
-                {id: 1, message: 'Hi, How are u ?', likesCount: 12, DislikesCount: 2},
-                {id: 2, message: 'It is my first post!)', likesCount: 78, DislikesCount: 0},
-                {id: 3, message: 'Я тут!'}
-            ],
-            newPostText: ''
+
+    getState() { return this._state; },
+
+    _callSubscriber() { console.log('test') },
+
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+            DislikesCount: 0
+        }
+        this._state.profilePage.postsData.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state);
     },
-    sidebar: {},
-}
 
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText 
+        this._callSubscriber(this._state);
+    },
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-        DislikesCount: 0
+    subscribe(observer) { // Observer - наблюдатель, паттерн
+        this._callSubscriber = observer
     }
-    state.profilePage.postsData.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => { // Observer - наблюдатель, паттерн
-    rerenderEntireTree = observer
-}
-
-export {state}
+export {store}
+window.store = store
--- react_social-network/src/index.js
+++ react_social-network/src/index.js
@@ -1,21 +1,24 @@
-import { state, subscribe } from './State';
+import { store } from './State';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText } from './State';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/> 
+      {/*object.bind(store) - это передали указания 
+в каком контексте выполнять функцию, из-за того, что мы 
+передаём функцию как callback, контекст изначальный (под store) теряется,
+поэтому когда функция addPost будет вызываться в логике, где прописано this._state будет undefined,
+т.к. контекст исполнения был утерян, поэтому при помощи bind(context) указываем, в контексте какого объекта ему выполняться
+в данной случае объекта store */}
     </React.StrictMode>
   );
 }
 
-rerenderEntireTree()
+rerenderEntireTree(store.getState());
 
-rerenderEntireTree(state);
-
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
